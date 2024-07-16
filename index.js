b2c5fbf8c874716db0a7c39878dc071794faced1
Juboraj Sujon <mdsujon72@gmail.com>
2024-07-16 21:05:23 +0600

    User registation done

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-// backend/server.js
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 dotenv.config();
 
@@ -31,12 +32,85 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    const db = client.db("mfsdb");
-    const usersCollection = db.collection("users");
+    const db = client.db(process.env.DB_NAME);
+    const userCollection = db.collection("users");
+    console.log("You successfully connected to MongoDB!");
 
-    app.use((req, res, next) => {
-      req.db = db;
+    // jwt related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.JWT_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+
+    // middleware
+    const verifyToken = (req, res, next) => {
+      // console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorized access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "unauthorized access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
+    // use verify admin after verifyToken
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
       next();
+    };
+
+    app.put("/auth/register", async (req, res) => {
+      const user = req.body;
+      const filter = { mobileNumber: user?.mobileNumber };
+
+      //if user already exists
+      const exists = await userCollection.findOne(filter);
+      if (exists) {
+        return res.send({
+          acknowledged: false,
+          message: "User already exists",
+        });
+      }
+
+      const options = { upsert: true };
+
+      //Hash the password
+      const hassedPassword = await bcrypt.hash(String(user.pin), 10);
+
+      // Store user in the database
+
+      const updateDoc = {
+        $set: {
+          name: user?.name,
+          email: user?.email,
+          mobileNumber: user?.mobileNumber,
+          pin: hassedPassword,
+          lastLogin: user?.lastLogin,
+        },
+        $setOnInsert: {
+          role: user?.role,
+          status: user?.status,
+          balance: user?.balance,
+          createdAt: user?.createdAt,
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+
+      res.send(result);
     });
 
     console.log("You successfully connected to MongoDB!");
